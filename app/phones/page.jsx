e03bbc14b3007bcafb796f91e5550c57f1247a7b
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2025-08-30 23:06:53 +0300

    Add new files and updates

--- app/phones/page.jsx
+++ app/phones/page.jsx
@@ -142,8 +142,8 @@ const handlePrintLabel = (product) => {
         <script src="https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js"></script>
         <style>
           @page {
-            size: 50mm 30mm;   /* مقاس ثابت لليبل */
-            margin: 0;        
+              size: 30mm 50mm portrait;
+              margin: 0;
           }
           body {
             margin: 0;
